bd59fd15be210dbcbbf52e6235fa4db8a490d1e3
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-06-26 14:30:20 +0000

    deploy

--- app-src/src/Components/ImageViewer.tsx
+++ app-src/src/Components/ImageViewer.tsx
@@ -1,24 +1,30 @@
+import { useRef, useState } from 'react'
 import { Grid } from './Grid'
 import './MoviePage.css'
 
 export function ImageViewer({ images }: { images: string[] }) {
+    const [mirrored, setMirrored] = useState(false)
+    const [currentImageSize, setCurrentImageSize] = useState()
     return (
         <Grid>
             {images?.map((img, idx) => {
                 return (
                     <div key={img}>
-                        <button>
-                            <img
-                                src={img}
-                                key={img}
-                                onClick={() => {
-                                    const el = document.getElementById(
-                                        img
-                                    ) as HTMLDialogElement
-                                    el.showModal()
-                                }}
-                            />
-                        </button>
+                        {/* <button> */}
+                        <img
+                            className="button"
+                            src={img}
+                            key={img}
+                            onClick={() => {
+                                setMirrored(false)
+                                const el = document.getElementById(
+                                    img
+                                ) as HTMLDialogElement
+                                el.showModal()
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        />
+                        {/* </button> */}
                         <dialog
                             className="dialog-screenshot"
                             id={img}
@@ -57,17 +63,38 @@ export function ImageViewer({ images }: { images: string[] }) {
                                     height: '100%',
                                     width: '100%',
                                 }}
+                                key={String(mirrored)}
                             >
                                 <form method="dialog" className="no-scrollbar">
                                     <button
                                         style={{
                                             position: 'absolute',
+                                            transform: 'translate(-000%,00%)',
                                             top: '1rem',
                                             left: '1rem',
+                                            zIndex: 5,
                                         }}
                                         className="material-icons"
                                     >
                                         arrow_back
+                                    </button>
+                                    <button
+                                        style={{
+                                            position: 'absolute',
+                                            top: '1rem',
+                                            right: '1rem',
+                                            zIndex: 5,
+                                            transform: mirrored
+                                                ? 'scaleX(-1)'
+                                                : undefined,
+                                        }}
+                                        className="material-icons"
+                                        onClick={(ev) => {
+                                            ev.preventDefault()
+                                            setMirrored((old) => !old)
+                                        }}
+                                    >
+                                        flip
                                     </button>
                                 </form>
                                 {/* <div> */}
@@ -77,11 +104,17 @@ export function ImageViewer({ images }: { images: string[] }) {
                                     style={{
                                         placeSelf: 'center',
                                         objectFit: 'contain',
-                                        width: '100%',
-                                        height: '100%',
+                                        // width: '100%',
+                                        // height: '100%',
+                                        // outline: '5px solid green',
                                         maxHeight: '100vh',
                                         maxWidth: '100vw',
+                                        transform: mirrored
+                                            ? 'scaleX(-1)'
+                                            : undefined,
+                                        // backgroundColor: 'red',
                                     }}
+                                    // ref={currentImageRef}
                                 />
                                 {/* </div> */}
                             </div>
