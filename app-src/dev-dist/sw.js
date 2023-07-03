43b6377a147b7706eae2d38479e2058cc3a56bf9
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-07-03 00:40:48 +0000

    deploy

--- app-src/dev-dist/sw.js
+++ app-src/dev-dist/sw.js
@@ -13,133 +13,187 @@
 
 // If the loader is already loaded, just stop.
 if (!self.define) {
-  let registry = {};
+    let registry = {}
 
-  // Used for `eval` and `importScripts` where we can't get script URL by other means.
-  // In both cases, it's safe to use a global var because those functions are synchronous.
-  let nextDefineUri;
+    // Used for `eval` and `importScripts` where we can't get script URL by other means.
+    // In both cases, it's safe to use a global var because those functions are synchronous.
+    let nextDefineUri
 
-  const singleRequire = (uri, parentUri) => {
-    uri = new URL(uri + ".js", parentUri).href;
-    return registry[uri] || (
-      
-        new Promise(resolve => {
-          if ("document" in self) {
-            const script = document.createElement("script");
-            script.src = uri;
-            script.onload = resolve;
-            document.head.appendChild(script);
-          } else {
-            nextDefineUri = uri;
-            importScripts(uri);
-            resolve();
-          }
-        })
-      
-      .then(() => {
-        let promise = registry[uri];
-        if (!promise) {
-          throw new Error(`Module ${uri} didn’t register its module`);
-        }
-        return promise;
-      })
-    );
-  };
-
-  self.define = (depsNames, factory) => {
-    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
-    if (registry[uri]) {
-      // Module is already loading or loaded.
-      return;
+    const singleRequire = (uri, parentUri) => {
+        uri = new URL(uri + '.js', parentUri).href
+        return (
+            registry[uri] ||
+            new Promise((resolve) => {
+                if ('document' in self) {
+                    const script = document.createElement('script')
+                    script.src = uri
+                    script.onload = resolve
+                    document.head.appendChild(script)
+                } else {
+                    nextDefineUri = uri
+                    importScripts(uri)
+                    resolve()
+                }
+            }).then(() => {
+                let promise = registry[uri]
+                if (!promise) {
+                    throw new Error(`Module ${uri} didn’t register its module`)
+                }
+                return promise
+            })
+        )
     }
-    let exports = {};
-    const require = depUri => singleRequire(depUri, uri);
-    const specialDeps = {
-      module: { uri },
-      exports,
-      require
-    };
-    registry[uri] = Promise.all(depsNames.map(
-      depName => specialDeps[depName] || require(depName)
-    )).then(deps => {
-      factory(...deps);
-      return exports;
-    });
-  };
+
+    self.define = (depsNames, factory) => {
+        const uri =
+            nextDefineUri ||
+            ('document' in self ? document.currentScript.src : '') ||
+            location.href
+        if (registry[uri]) {
+            // Module is already loading or loaded.
+            return
+        }
+        let exports = {}
+        const require = (depUri) => singleRequire(depUri, uri)
+        const specialDeps = {
+            module: { uri },
+            exports,
+            require,
+        }
+        registry[uri] = Promise.all(
+            depsNames.map((depName) => specialDeps[depName] || require(depName))
+        ).then((deps) => {
+            factory(...deps)
+            return exports
+        })
+    }
 }
-define(['./workbox-865c825e'], (function (workbox) { 'use strict';
+define(['./workbox-865c825e'], function (workbox) {
+    'use strict'
 
-  self.skipWaiting();
-  workbox.clientsClaim();
+    self.skipWaiting()
+    workbox.clientsClaim()
 
-  /**
-   * The precacheAndRoute() method efficiently caches and responds to
-   * requests for URLs in the manifest.
-   * See https://goo.gl/S9QRab
-   */
-  workbox.precacheAndRoute([{
-    "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
-  }, {
-    "revision": null,
-    "url": "/teikna/index.html"
-  }], {});
-  workbox.cleanupOutdatedCaches();
-  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/teikna/index.html"), {
-    allowlist: [/^\/$/]
-  }));
-  workbox.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i, new workbox.CacheFirst({
-    "cacheName": "google-fonts-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 31536000
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-  workbox.registerRoute(/^https:\/\/fonts\.gstatic\.com\/.*/i, new workbox.CacheFirst({
-    "cacheName": "gstatic-fonts-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 31536000
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-  workbox.registerRoute(/^https:\/\/film-grab\.com\/.*/i, new workbox.CacheFirst({
-    "cacheName": "filmgrab-images-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxAgeSeconds: 604800
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-  workbox.registerRoute(/^https:\/\/i\.ytimg\.com\/.*/i, new workbox.CacheFirst({
-    "cacheName": "video-thumbnails-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 604800
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-  workbox.registerRoute(/^https:\/\/i\.redd\.it\/.*/i, new workbox.CacheFirst({
-    "cacheName": "reddit-images-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 604800
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-  workbox.registerRoute(/^.*.gif.*/i, new workbox.CacheFirst({
-    "cacheName": "gif-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 604800
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-
-}));
+    /**
+     * The precacheAndRoute() method efficiently caches and responds to
+     * requests for URLs in the manifest.
+     * See https://goo.gl/S9QRab
+     */
+    workbox.precacheAndRoute(
+        [
+            {
+                url: 'registerSW.js',
+                revision: '3ca0b8505b4bec776b69afdba2768812',
+            },
+            {
+                revision: null,
+                url: '/index.html',
+            },
+        ],
+        {}
+    )
+    workbox.cleanupOutdatedCaches()
+    workbox.registerRoute(
+        new workbox.NavigationRoute(
+            workbox.createHandlerBoundToURL('/index.html'),
+            {
+                allowlist: [/^\/$/],
+            }
+        )
+    )
+    workbox.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com\/.*/i,
+        new workbox.CacheFirst({
+            cacheName: 'google-fonts-cache',
+            plugins: [
+                new workbox.ExpirationPlugin({
+                    maxEntries: 10,
+                    maxAgeSeconds: 31536000,
+                }),
+                new workbox.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        }),
+        'GET'
+    )
+    workbox.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com\/.*/i,
+        new workbox.CacheFirst({
+            cacheName: 'gstatic-fonts-cache',
+            plugins: [
+                new workbox.ExpirationPlugin({
+                    maxEntries: 10,
+                    maxAgeSeconds: 31536000,
+                }),
+                new workbox.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        }),
+        'GET'
+    )
+    workbox.registerRoute(
+        /^https:\/\/film-grab\.com\/.*/i,
+        new workbox.CacheFirst({
+            cacheName: 'filmgrab-images-cache',
+            plugins: [
+                new workbox.ExpirationPlugin({
+                    maxAgeSeconds: 604800,
+                }),
+                new workbox.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        }),
+        'GET'
+    )
+    workbox.registerRoute(
+        /^https:\/\/i\.ytimg\.com\/.*/i,
+        new workbox.CacheFirst({
+            cacheName: 'video-thumbnails-cache',
+            plugins: [
+                new workbox.ExpirationPlugin({
+                    maxEntries: 10,
+                    maxAgeSeconds: 604800,
+                }),
+                new workbox.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        }),
+        'GET'
+    )
+    workbox.registerRoute(
+        /^https:\/\/i\.redd\.it\/.*/i,
+        new workbox.CacheFirst({
+            cacheName: 'reddit-images-cache',
+            plugins: [
+                new workbox.ExpirationPlugin({
+                    maxEntries: 10,
+                    maxAgeSeconds: 604800,
+                }),
+                new workbox.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        }),
+        'GET'
+    )
+    workbox.registerRoute(
+        /^.*.gif.*/i,
+        new workbox.CacheFirst({
+            cacheName: 'gif-cache',
+            plugins: [
+                new workbox.ExpirationPlugin({
+                    maxEntries: 10,
+                    maxAgeSeconds: 604800,
+                }),
+                new workbox.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        }),
+        'GET'
+    )
+})
